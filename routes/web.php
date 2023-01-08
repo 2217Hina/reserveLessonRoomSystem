b4437947c542aeb0db0edfdc2be34bffe4cdd99e
2217Hina <hina.funatsu@gmail.com>
2023-01-08 12:45:38 +0900

    8-1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/rooms', [RoomController::class, 'index']);
+
+Route::get('/users',[UserController::class,'index']);
 
 Route::get('/', function () {
     return view('welcome');
