8618d86dcb22d4f394178493c67aa14b66621a03
2217Hina <hina.funatsu@gmail.com>
2023-01-21 19:02:47 +0900

    Breezeインストール

--- app/Http/Controllers/RLRController.php
+++ app/Http/Controllers/RLRController.php
@@ -1,19 +1,22 @@
 <?php
 
+
+
+
+
+
+
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
+
 use Illuminate\Http\Request;
 use App\Models\User;
 use Validator;
 use App\Http\Requests\RLRRequest;
 
-class RLRController extends BaseController
+class RLRController extends Controller
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    
     
     public function top()
     {
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RoomController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\RLRController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,18 +13,27 @@ use App\Http\Controllers\RLRController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/rooms', [RoomController::class, 'index']);
-Route::get('/users', [UserController::class, 'index']);
 
-Route::get('/rlr/top', [RLRController::class, 'top']);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
 
-Route::get('/rlr/register', [RLRController::class, 'register']);
 
-Route::POST('/rlr/register/confirm',[RLRController::class, 'confirm']);
 
-Route::get('/rlr/register/confirm/{user}',[RLRController::class, 'confirmUserInfo']);
 
-Route::POST('/rlr/register/complete',[RLRController::class, 'complete']);
-
-Route::get('/rlr/myPage', [RLRController::class, 'myPage']);
+Route::get('/rlr/myPage', function () {
+    return view('RLR/myPage');
+});
